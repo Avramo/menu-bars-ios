988c277e099b9942a37b3c5cf1ae74c0bf565218
Avraham Kirsch <etrog95@gmail.com>
2020-09-29 14:21:15 +0300

    first commit

--- Menu Bars/ViewController.swift
+++ Menu Bars/ViewController.swift
@@ -13,8 +13,62 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
+    var timer: Timer?
+    
+    var time = 210
+    
+    
+    @IBOutlet weak var timeLabel: UILabel!
 
+    @objc func startTimer() {
+        
+        if time > 0 {
+            time -= 1
+            timeLabel.text = String(time)
+        }
+        else {
+          resetTimer()
+        }
+    }
+    
+    func resetTimer() {
+        timer?.invalidate()
+        timer = nil
+        time = 210
+        timeLabel.text = String(time)
+    }
+    
+    @IBAction func playTimer(_ sender: UIBarButtonItem) {
+        if timer == nil {
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.startTimer), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @IBAction func pauseTimer(_ sender: UIBarButtonItem) {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    @IBAction func resetTimer(_ sender: Any) {
+        resetTimer()
+    }
+    
+    @IBAction func updateTime(_ sender: Any) {
+        if timer != nil{
+            if (sender as! UIBarButtonItem).tag == 0{
+                time -= 10
+            }
+            else {
+                time += 10
+            }
+        }
+    }
+    
+    
+
+    
 }
 
